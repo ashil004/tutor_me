d3c05dbf6d0ce7bd4127df61442512c40a7d47f5
ashil004 <pramanikashik007@gmail.com>
2023-11-04 23:17:16 +0600

    add to the error massage in the client site

--- src/assets/error Massage/Error.jsx
+++ src/assets/error Massage/Error.jsx
@@ -1,10 +1,21 @@
+import { Link } from "react-router-dom";
+
 
 
 const Error = () => {
     return (
-        <div>
-            Hello error
+        <div className="hero min-h-screen bg-base-200 " style={{backgroundImage: 'url(https://i.ibb.co/SdmNzRP/404-not-found.gif)'}} >
+            
+      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="text-center lg:text-left">
+      <h3 className="text-5xl text-white font-bold "> Error Page 404 </h3>
+      <div className="flex justify-center p-4">
+        <Link to={'/'}><button className="btn  btn-outline btn-error"><span className="text-white">Go to Home</span></button></Link>
         </div>
+    </div>
+    
+  </div>
+</div>
     );
 };
 
